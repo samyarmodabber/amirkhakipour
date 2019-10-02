ffd90f48cbd8279cfa5490e3093fe9f9d1c76ede
Samyar <samyar.modabber@gmail.com>
2019-10-02 21:48:29 +0200

    build client

---
+++ client/build/precache-manifest.c715bfd160ace2280ac4e3688ef9f926.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6095cc99dfdf340fd572fc7ec39c0e37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4a543a1c4e458e4fba2",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "7d34aff495853d3be077",
+    "url": "/static/js/2.97b5b5a7.chunk.js"
+  },
+  {
+    "revision": "e4a543a1c4e458e4fba2",
+    "url": "/static/js/main.e28cdb2d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
